a64931d9dce7b42b575308363bc7927f70d6dc3e
gratel <531129143@qq.com>
2022-12-07 21:56:18 -0500

    backend bash script.

---
+++ mini_project/setup.bash
@@ -0,0 +1,16 @@
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+
+conda update conda
+conda create -n 655mini
+conda activate 655mini
+
+pip install -r requirement.txt
+
+
+
+python app.py
